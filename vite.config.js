592b9561eeb71053ade0854af6cc05962023d7e4
zee1021 <suzzzanenalibsam@gmail.com>
2026-04-13 21:52:45 +0800

    Updated file for tailwind and postcss.

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from '@tailwindcss/vite'; // Import the plugin
 
 export default defineConfig({
     plugins: [
@@ -8,11 +8,6 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        tailwindcss(),
+        tailwindcss(), // Add this line
     ],
-    server: {
-        watch: {
-            ignored: ['**/storage/framework/views/**'],
-        },
-    },
 });
