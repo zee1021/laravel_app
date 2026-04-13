e61cd3641518aeea74fa7abf081892b686502608
zee1021 <suzzzanenalibsam@gmail.com>
2026-04-13 22:16:23 +0800

    Install Laravel Breeze and scaffold authentication system

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite'; // Import the plugin
 
 export default defineConfig({
     plugins: [
@@ -8,6 +7,5 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        tailwindcss(), // Add this line
     ],
 });
