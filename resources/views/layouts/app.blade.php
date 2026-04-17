2e0896ba524a53a3613e8796fc9984d46f9a719f
zee1021 <suzzzanenalibsam@gmail.com>
2026-04-18 05:44:03 +0800

    Added page header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,6 +24,15 @@
         </div>
     </nav>
 
+    <!-- PAGE HEADER -->
+    @isset($header)
+        <header class="bg-white shadow-sm">
+            <div class="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8">
+                {{ $header }}
+            </div>
+        </header>
+    @endisset
+
     <!-- CONTENT -->
     <main class="p-6">
         {{ $slot }}
